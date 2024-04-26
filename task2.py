cebbec30fe0eae8ab36c61bd71a6fccb4d81657f
KTGSYarik123 <166371018+KTGSYarik123@users.noreply.github.com>
2024-04-26 11:08:14 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,24 @@
+'''a = 1, 2, 3
+b = str(a)
+print(b)
+print(type(b))
+
+
+a = 1, 5.5, "hello"
+b = list(a)
+print(b)
+print(type(b))
+
+
+a = int(input("Введите число"))
+if a % 2:
+    print (f"число {a} является нечетным")
+else:
+    print (f"число {a} является четным")'''
+
+
+ya = input("Введите строку:")
+po = len(ya)
+print(f"Количество символов с строке: {po}")
+
+
